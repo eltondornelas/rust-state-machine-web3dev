0bd3fa71a184cb53d6f11ccb516651ea31a83223
eltondornelas <eltondornelas@gmail.com>
2024-11-29 09:04:23 -0300

    criando metodo para executar os blocos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use support::Dispatch;
+
 mod balances;
 mod support;
 mod system;
@@ -39,6 +41,55 @@ impl Runtime {
             balances: balances::Pallet::new(),
             system: system::Pallet::new(),
         }
+    }
+
+    // Execute a block of extrinsics. Increments the block number.
+    fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
+        self.system.increment_block_number();
+
+        if block.header.block_number != self.system.block_number() {
+            return Err("Block number mismatch");
+        }
+
+        // iter: This method creates an iterator that borrows each element from the vector, allowing you to read the values without taking ownership. It's useful when you want to iterate over the vector while keeping it intact.
+        // into_iter: This method consumes the vector, transferring ownership of each element to the iterator. It's handy when you want to move or transfer ownership of the vector's elements to another part of your code. After using into_iter, the original vector can't be used anymore, as ownership has been transferred.
+        for (i, support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
+            self.system.increment_nonce(&caller);
+
+            let res = self.dispatch(caller, call).map_err(|e| {
+                format!(
+                    "Error in block {}: extrinsic {}: {}",
+                    block.header.block_number, i, e
+                )
+            });
+        }
+
+        /*
+        * let _res = self.dispatch(caller, call).map_err(|e| eprintln!("{}", e));
+        * eprintln!(
+           "Extrinsic Error\n\tBlock Number: {}\n\tExtrinsic Number: {}\n\tError: {}",
+           block.header.block_number, i, e
+          )
+        */
+
+        Ok(())
+    }
+}
+
+impl crate::support::Dispatch for Runtime {
+    type Caller = <Runtime as system::Config>::AccountId;
+    type Call = RuntimeCall;
+    // Dispatch a call on behalf of a caller. Increments the caller's nonce.
+    //
+    // Dispatch allows us to identify which underlying module call we want to execute.
+    // Note that we extract the `caller` from the extrinsic, and use that information
+    // to determine who we are executing the call on behalf of.
+    fn dispatch(
+        &mut self,
+        caller: Self::Caller,
+        runtime_call: Self::Call,
+    ) -> support::DispatchResult {
+        unimplemented!();
     }
 }
 
